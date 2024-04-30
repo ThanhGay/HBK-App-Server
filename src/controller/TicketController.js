e1f749893f1efb5636f6f5c8e93dbde6657b98c7
DucThanh <phoducthanh@gmail.com>
2024-04-30 23:08:59 +0700

    edit api getSeats by movieId

--- src/controller/TicketController.js
+++ src/controller/TicketController.js
@@ -11,25 +11,7 @@ const getDBTicket = {
       const pool = await connection;
       const request = pool.request();
 
-      const query =
-        // `Begin Transaction Booking
-        // Insert into Invoice
-        // Values (GetDate(), 0, @PhoneNumber)
-        // DECLARE @Const Int
-        // set @Const = @rollBack
-        // IF @Const = 0
-        // BEGIN
-        //     ROLLBACK TRANSACTION Booking;
-        //     PRINT 'rollBack thành công';
-        // END
-        // ELSE
-        // BEGIN
-        //     COMMIT TRANSACTION Booking;
-        //     PRINT 'Commit thành công';
-        // END;
-        // SELECT SCOPE_IDENTITY() AS NewInvoiceId;
-        // Select * from Invoice`
-        `Insert into Invoice
+      const query = `Insert into Invoice
             Values (GetDate(), 0, @PhoneNumber)
             SELECT SCOPE_IDENTITY() AS NewInvoiceId;`;
 
@@ -49,8 +31,8 @@ const getDBTicket = {
     try {
       const pool = await connection;
       const request = pool.request();
-      const query1 = `Select STRING_AGG(Seat_Id, ', ') as Reserved
-            From Ticket 
+      const query1 = `Select CONCAT('[', STRING_AGG(CONCAT('"', Seat_Id, '"'), ', '), ']')  as Reserved
+            From Ticket
             where StartTime = @StartTime`;
       const query2 = `Select Room_Id
       From MovieShow
@@ -144,7 +126,7 @@ const getDBTicket = {
       const pool = await connection;
       const request = pool.request();
       const query = `Insert into MyTicket
-        Values (@Invoice_Id, @Movie_Name, @Duration, @CategoryList, @Poster, @StartTime, @Room_Id, @Seat_Id, @Price, @PhoneNumber)`;
+        Values (@Invoice_Id, @Movie_Name, @Duration, @CategoryList, @Poster, @StartTime, @InvoiceDate, @Room_Id, @Seat_Id, @Price, @PhoneNumber)`;
       for (key in data) {
         request.input(key, data[key]);
       }
